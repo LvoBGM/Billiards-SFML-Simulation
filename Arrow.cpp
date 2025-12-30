b933f0d98eccfc32930ae506d9cb3072cf4ac22e
bulgarmapper@gmail.com <bulgarmapper@gmail.com>
2025-12-30 22:55:43 +0200

    Implement the setVector function

--- Arrow.cpp
+++ Arrow.cpp
@@ -1,4 +1,8 @@
 #include "Arrow.h"
+void Arrow::setVector(const sf::Vector2f& v) {
+	m_vector = v;
+}
+
 void Arrow::setColor(const sf::Color & color) {
 	for (std::size_t i = 0; i < m_vertices.getVertexCount(); ++i)
 	{
