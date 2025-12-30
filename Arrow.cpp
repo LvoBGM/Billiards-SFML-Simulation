35103c929840673789255c66ae7a7bc9d44e2d38
bulgarmapper@gmail.com <bulgarmapper@gmail.com>
2025-12-30 22:53:31 +0200

    implements the setColor function

--- Arrow.cpp
+++ Arrow.cpp
@@ -1,1 +1,7 @@
 #include "Arrow.h"
+void Arrow::setColor(const sf::Color & color) {
+	for (std::size_t i = 0; i < m_vertices.getVertexCount(); ++i)
+	{
+		m_vertices[i].color = sf::Color::Red;
+	}
+}
